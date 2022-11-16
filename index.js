3f300869311cf0db58d5709cd2a8aa1dee279e34
Andres <andres.romero1@correo.usa.edu.co>
2022-11-16 12:24:25 -0500

    enrutamiento con Express

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola server desde Express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy un nuevo EndPoint');
+})
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  });
+})
+
 app.listen(port, () => {
   console.log('Mi port ' + port)
 });
